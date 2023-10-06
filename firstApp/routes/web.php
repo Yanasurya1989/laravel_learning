4562d44433ad6dc8e48cba036fa7241426163345
YanaWebProgrammer <teukita15@gmail.com>
2023-10-06 16:57:32 +0700

    member controller and model

--- firstApp/routes/web.php
+++ firstApp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Master_controller;
+use App\Http\Controllers\Member_controller;
 use App\Http\Controllers\Product_controller;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +20,15 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// product
 Route::get('/', [Product_controller::class, 'index']);
 Route::get('/product/insert', [Product_controller::class, 'create']);
 Route::post('/product/add', [Product_controller::class, 'store']);
 Route::get('/delete/product/{product_model}', [Product_controller::class, 'destroy']);
 Route::get('/edit/product/{product_model}', [Product_controller::class, 'edit']);
 Route::post('/update/product/{product_model}', [Product_controller::class, 'update']);
+
+// member
+Route::get('/user', [Member_controller::class, 'index']);
 
 Route::get('/master', [Master_controller::class, 'index_master']);
